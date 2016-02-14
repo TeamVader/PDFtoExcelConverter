7b4c37a2b8564440f7aba42303ba3790c562bc04
TeamVader <alex.stark@hotmail.com>
2016-02-14 14:51:40 +0100

    Create repositories

---
+++ PDFtoExcelConverter/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace PDFtoExcelConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttopen = new System.Windows.Forms.Button();
+            this.buttconvert = new System.Windows.Forms.Button();
+            this.labelname = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbocopies = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttopen
+            // 
+            this.buttopen.Location = new System.Drawing.Point(12, 69);
+            this.buttopen.Name = "buttopen";
+            this.buttopen.Size = new System.Drawing.Size(108, 33);
+            this.buttopen.TabIndex = 0;
+            this.buttopen.Text = "open PDF ";
+            this.buttopen.UseVisualStyleBackColor = true;
+            this.buttopen.Click += new System.EventHandler(this.buttopen_Click);
+            // 
+            // buttconvert
+            // 
+            this.buttconvert.Location = new System.Drawing.Point(13, 109);
+            this.buttconvert.Name = "buttconvert";
+            this.buttconvert.Size = new System.Drawing.Size(107, 32);
+            this.buttconvert.TabIndex = 1;
+            this.buttconvert.Text = "convert";
+            this.buttconvert.UseVisualStyleBackColor = true;
+            this.buttconvert.Click += new System.EventHandler(this.buttconvert_Click);
+            // 
+            // labelname
+            // 
+            this.labelname.AutoSize = true;
+            this.labelname.Location = new System.Drawing.Point(13, 13);
+            this.labelname.Name = "labelname";
+            this.labelname.Size = new System.Drawing.Size(0, 17);
+            this.labelname.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::PDFtoExcelConverter.Properties.Resources.Stark_Industries_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(157, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(476, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "No of Copies";
+            // 
+            // cbocopies
+            // 
+            this.cbocopies.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbocopies.Location = new System.Drawing.Point(16, 177);
+            this.cbocopies.Name = "cbocopies";
+            this.cbocopies.Size = new System.Drawing.Size(104, 24);
+            this.cbocopies.TabIndex = 5;
+            this.cbocopies.SelectedIndexChanged += new System.EventHandler(this.cbocopies_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 218);
+            this.Controls.Add(this.cbocopies);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelname);
+            this.Controls.Add(this.buttconvert);
+            this.Controls.Add(this.buttopen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "PDF to Excel Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttopen;
+        private System.Windows.Forms.Button buttconvert;
+        private System.Windows.Forms.Label labelname;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbocopies;
+    }
+}
+
---
+++ PDFtoExcelConverter/Form1.cs
@@ -0,0 +1,234 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using PDFtoExcelConverter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace PDFtoExcelConverter
+{
+    public partial class Form1 : Form
+    {
+        private bool File_Selected;
+        private string path_to_pdf;
+        private Int32 no_copies;
+        private string startup_path;
+
+        private string Exceltemplate = "BMK-Vorlage.xls";
+        public Form1()
+        {
+            InitializeComponent();
+            File_Selected = false;
+            path_to_pdf = "";
+            buttconvert.Enabled = false;
+            cbocopies.Items.AddRange(new string[] { "1", "2" ,"3","4","5"});
+            no_copies = 1;
+            cbocopies.SelectedIndex = 0;
+            startup_path = (System.Windows.Forms.Application.StartupPath);
+           // MessageBox.Show(startup_path);
+            
+        }
+
+        private void buttopen_Click(object sender, EventArgs e)
+        {
+           
+         OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+         openFileDialog1.InitialDirectory = "c:\\" ;
+          openFileDialog1.Filter = "PDF (*.pdf)|*.pdf" ;
+          openFileDialog1.FilterIndex = 2 ;
+          openFileDialog1.RestoreDirectory = true ;
+          openFileDialog1.Multiselect = false;
+
+          if (openFileDialog1.ShowDialog() == DialogResult.OK)
+          {
+              path_to_pdf = openFileDialog1.FileName;
+              if (System.IO.Path.GetExtension(path_to_pdf) == ".pdf")
+              {
+                  File_Selected=true;
+                  buttconvert.Enabled=true;
+                  labelname.Text = System.IO.Path.GetFileNameWithoutExtension(path_to_pdf);
+              }
+              else
+              {
+                  File_Selected = false;
+                      buttconvert.Enabled=false;
+                      labelname.Text = "No PDF!!";
+              }
+          }
+          else
+          {
+              //File_Selected = false;
+              buttconvert.Enabled = false;
+              labelname.Text = "";
+          }
+}
+
+        private void cbocopies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           // MessageBox.Show(cbocopies.SelectedItem.ToString());
+            no_copies = Int32.Parse(cbocopies.SelectedItem.ToString());
+        }
+
+        private void buttconvert_Click(object sender, EventArgs e)
+        {
+
+            Regex bmk_regex = new Regex(@"[-+]?([0-9]*\.[0-9]+|[0-9]+)[A-Z][-+]?([0-9]*\.[0-9]+|[0-9]+)");
+            Match match;
+            try
+            {
+
+                 Microsoft.Office.Interop.Excel.Application excel_app = new Microsoft.Office.Interop.Excel.Application();
+                 Range worksheet_range;
+                // Make Excel visible (optional).
+                 excel_app.Visible = false;
+                 excel_app.DisplayAlerts = false;
+                int rowpointer=0;
+                int columnpointer =0;
+                int minrow = 2;
+                int mincolumn = 3;
+                int maxrow=43;
+                int maxcolumn=21;
+                int sheet_number = 1;
+                string[] resultstring;
+                
+                var text = new StringBuilder();
+
+                // The PdfReader object implements IDisposable.Dispose, so you can
+                // wrap it in the using keyword to automatically dispose of it
+                using (var pdfReader = new PdfReader(path_to_pdf))
+                {
+                    // Loop through each page of the document
+                    for (var page = 1; page <= pdfReader.NumberOfPages; page++)
+                    {
+                        ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+
+                        var currentText = PdfTextExtractor.GetTextFromPage(
+                            pdfReader,
+                            page,
+                            strategy);
+
+                        currentText =
+                            Encoding.UTF8.GetString(Encoding.Convert(
+                                Encoding.Default,
+                                Encoding.UTF8,
+                                Encoding.Default.GetBytes(currentText)));
+
+                        text.Append(currentText);
+                    }
+
+                   // MessageBox.Show(startup_path + "\\" + Exceltemplate);
+                    if(File.Exists(startup_path+"\\"+Exceltemplate))
+                    {
+                    Microsoft.Office.Interop.Excel.Workbooks workbooks = excel_app.Workbooks;
+
+                    Microsoft.Office.Interop.Excel.Workbook workbook = workbooks.Open(
+                    (startup_path+"\\"+Exceltemplate));
+
+                    // Get the first worksheet.
+                    Microsoft.Office.Interop.Excel.Sheets sheets = workbook.Sheets;
+                    Microsoft.Office.Interop.Excel.Worksheet sheet_template = sheets["Vorlage"];
+
+                    rowpointer = minrow;
+                    columnpointer = mincolumn;
+                    
+                    if (sheet_template != null)
+                    {
+                        resultstring = text.ToString().Split(new char[]{' '});
+                        for (int i = 0; i < resultstring.Length; i++)
+                        {
+                            
+                            if (columnpointer > maxcolumn)
+                            {
+                                columnpointer = mincolumn;
+                                rowpointer += no_copies;
+                            }
+
+                            if ((no_copies + rowpointer) > maxrow)
+                            {
+                                workbook.SaveAs(System.IO.Path.GetDirectoryName(path_to_pdf) + "\\" + System.IO.Path.GetFileNameWithoutExtension(path_to_pdf) + "_" + sheet_number.ToString() + ".xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+                                for (int l = minrow; l == maxrow; l++)
+                                {
+                                    for (int n = mincolumn; n == maxcolumn; n++)
+                                    {
+                                        sheet_template.Cells[l, n] = "";
+                                    }
+                                }
+                                sheet_number++;
+                                rowpointer = minrow;
+                            }
+                            match = bmk_regex.Match(resultstring[i]);
+                            if (match.Success)
+                            {
+                                for (int k = 0; k < no_copies; k++)
+                                {
+
+                                    sheet_template.Cells[rowpointer + k, columnpointer] = match.Value;
+                                  //  MessageBox.Show(columnpointer.ToString() + " Reihe" + rowpointer.ToString());
+                                    
+
+
+                                }
+                                columnpointer += 2;
+                            }
+                          
+                           
+                        }
+                       
+                    }
+
+                    workbook.SaveAs(System.IO.Path.GetDirectoryName(path_to_pdf) + "\\" + System.IO.Path.GetFileNameWithoutExtension(path_to_pdf) + "_" + sheet_number.ToString() + ".xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+
+
+                    // workbook.ExportAsFixedFormat(XlFixedFormatType.xlTypePDF, excel_path[0] + "_bom.pdf");
+                    // Close the workbook without saving changes.
+                    labelname.Text = "File successfully converted";
+                   
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(sheet_template);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(sheets);
+                    workbook.Close(0);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(workbooks);
+                    excel_app.Quit();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excel_app);
+
+                    foreach (Process process in Process.GetProcessesByName("Excel"))
+                    {
+                        if (!string.IsNullOrEmpty(process.ProcessName) && process.StartTime.AddSeconds(+10) > DateTime.Now)
+                        {
+                            process.Kill();
+                        }
+                    }
+                    }
+                    else
+                    {
+                        labelname.Text="No Template in Folder";
+                    }
+                    //MessageBox.Show(text.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        
+    }
+}
